f7342371fd2115df97cb02fd3c67427f5a2a7216
Jonathan Tielve <TielveJonathan@gmail.com>
2025-05-16 20:59:42 -0400

    Added Epsilon Greedy Q

--- src/main.c
+++ src/main.c
@@ -7,9 +7,13 @@
 #define ACTION_COUNT 4
 #define STATE_COUNT (MAZE_WIDTH * MAZE_HEIGHT)
 #define STACK_SIZE 1024
-#define SENSOR_PRIORITY   3
-#define AI_PRIORITY       2
+#define SENSOR_PRIORITY   2
+#define AI_PRIORITY       1
 #define ACTUATOR_PRIORITY 3
+#define EPSILON 0.1f
+#define GAMMA 0.9f
+#define ALPHA 0.1f
+
 
 void sensor_thread(void *a, void *b, void *c);
 void ai_thread(void *a, void *b, void *c);
@@ -105,39 +109,104 @@ int get_state_index(position_t pos) {
     return pos.y * MAZE_WIDTH + pos.x;
 }
 
+action_t choose_action(position_t pos) {
+    int state = get_state_index(pos);
+
+    //When less than epsilon explore, get random action
+    if ((float)sys_rand32_get() / UINT32_MAX < EPSILON) {
+        return (action_t)(sys_rand32_get() % ACTION_COUNT);
+    }
+
+    //Make max_q a temp arbitrarily low number, and make best move an arbitrary action
+    float max_q = -9999.0f;
+    action_t best = MOVE_UP;
+
+    //Find best action based on Q table
+    for (int i = 0; i < ACTION_COUNT; i++) {
+        if (q_table[state][i] > max_q) {
+            max_q = q_table[state][i];
+            best = (action_t)i;
+        }
+    }
+    return best;
+}
+
+void update_q(position_t old_pos, action_t action, position_t new_pos, float reward) {
+    int s = get_state_index(old_pos);
+    int s_prime = get_state_index(new_pos);
+
+    //Get first Max Q to compare with in for loop
+    float max_q_next = q_table[s_prime][0];
+    for (int i = 1; i < ACTION_COUNT; i++) {
+        if (q_table[s_prime][i] > max_q_next) {
+            max_q_next = q_table[s_prime][i];
+        }
+    }
+
+    //Q-learning update formula
+    q_table[s][action] += ALPHA * (reward + GAMMA * max_q_next - q_table[s][action]);
+}
+
+position_t get_resulting_pos(position_t pos, action_cmd_t cmd) {
+    position_t result = pos;
+    if (cmd.action == MOVE_UP && agent_pos.y > 0 && !maze[agent_pos.y - 1][agent_pos.x]) {
+        result.y--;
+    }
+    if (cmd.action == MOVE_DOWN && agent_pos.y < MAZE_HEIGHT - 1 && !maze[agent_pos.y + 1][agent_pos.x]) {
+        result.y++;
+    }
+    if (cmd.action == MOVE_LEFT && agent_pos.x > 0 && !maze[agent_pos.y][agent_pos.x - 1]) {
+        result.x--;
+    }
+    if (cmd.action == MOVE_RIGHT && agent_pos.x < MAZE_WIDTH - 1 && !maze[agent_pos.y][agent_pos.x + 1]) {
+        result.x++;
+    }
+    return result;
+}
+
+float get_reward(position_t old_pos, position_t new_pos) {
+    //Reached goal
+    if (new_pos.x == goal_pos.x && new_pos.y == goal_pos.y) {
+        return 100.0f;
+    }
+
+    //Invalid move
+    if (new_pos.x == old_pos.x && new_pos.y == old_pos.y) {
+        return -10.0f;
+    }
+
+    //Any other valid move
+    return -1.0f;
+}
 
 void ai_thread(void *a, void *b, void *c) {
     sensor_data_t sensors;
     action_cmd_t cmd;
     position_t new_pos;
+    position_t old_pos;
+    float reward;
 
     while (1) {
         k_msgq_get(&sensor_queue, &sensors, K_FOREVER);
+        old_pos = agent_pos;
+        cmd.action = choose_action(old_pos);
 
-        //Randomize actions and restart when at goal
-        do {
-            cmd.action = sys_rand32_get() % ACTION_COUNT;
+        new_pos = get_resulting_pos(old_pos, cmd);
+        reward = get_reward(old_pos, new_pos);
+        update_q(old_pos, cmd.action, new_pos, reward);
 
-            new_pos = agent_pos;
-            if (cmd.action == MOVE_UP) {
-                new_pos.y--;
-            }
-            if (cmd.action == MOVE_DOWN) {
-                new_pos.y++;
-            }
-            if (cmd.action == MOVE_LEFT) {
-                new_pos.x--;
-            }
-            if (cmd.action == MOVE_RIGHT) {
-                new_pos.x++;
-            }
-        } while (
-            new_pos.x < 0 || new_pos.x >= MAZE_WIDTH ||
-            new_pos.y < 0 || new_pos.y >= MAZE_HEIGHT ||
-            maze[new_pos.y][new_pos.x] == 1
-        );
+        if (reward != -10) {
+            agent_pos = new_pos;
+        }
 
         k_msgq_put(&action_queue, &cmd, K_FOREVER);
+
+        if (agent_pos.x == MAZE_WIDTH - 1 && agent_pos.y == MAZE_HEIGHT - 1) {
+            agent_pos.x = 0;
+            agent_pos.y = 0;
+        }
+
+        k_msleep(100);
     }
 }
 
@@ -145,16 +214,6 @@ void actuator_thread(void *a, void *b, void *c) {
     action_cmd_t cmd;
     while (1) {
         k_msgq_get(&action_queue, &cmd, K_FOREVER);
-
-        if (cmd.action == MOVE_UP && agent_pos.y > 0 && !maze[agent_pos.y - 1][agent_pos.x])
-            agent_pos.y--;
-        if (cmd.action == MOVE_DOWN && agent_pos.y < MAZE_HEIGHT - 1 && !maze[agent_pos.y + 1][agent_pos.x])
-            agent_pos.y++;
-        if (cmd.action == MOVE_LEFT && agent_pos.x > 0 && !maze[agent_pos.y][agent_pos.x - 1])
-            agent_pos.x--;
-        if (cmd.action == MOVE_RIGHT && agent_pos.x < MAZE_WIDTH - 1 && !maze[agent_pos.y][agent_pos.x + 1])
-            agent_pos.x++;
-
         printk("Agent position: (%d, %d)\n", agent_pos.x, agent_pos.y);
     }
 }
